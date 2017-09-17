3c970452db3f005f3637d293c34b9dc66b8301d7
mwillia95 <mwilliams10@augusta.edu>
2017-09-17 06:14:39 -0400

    putting acm-website on personal repository temporarily

---
+++ ACM-Website/Source/ACM-Website/Controllers/GameDevController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACM_Website.Controllers
+{
+    public class GameDevController : Controller
+    {   //Game Dev
+        // GET: GameDev
+        // ActionResult returns HTML
+        public ActionResult Index()
+        {
+            ViewBag.Whatever = "Whatever";
+            return View();
+        }
+        public ActionResult Resources()
+        {
+            return View();
+        }
+        public ActionResult Agenda()
+        {
+            return View();
+        }
+        public ActionResult Event()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+    }
+}
---
+++ ACM-Website/Source/ACM-Website/Controllers/WebDevController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACM_Website.Controllers
+{
+    public class WebDevController : Controller
+    {   //Shelby
+        // GET: WebDev
+        // ActionResult returns HTML
+        public ActionResult Index()
+        {
+            //Picture - Jumbotron
+            //Description
+            return View();
+        }
+        public ActionResult Resources()
+        {
+            //Documents
+            //Resources
+            return View();
+        }
+        public ActionResult Agenda()
+        {
+            // Most recent thing to do - block
+            //Past agenda - list
+            return View();
+        }
+        public ActionResult Event()
+        {
+            //Upcoming list
+            //History record event
+            //Google Calendar
+            return View();
+        }
+        public ActionResult Info()
+        {
+            //Captain -Members
+            //Meetings times -Location
+            //Description
+            //Contact information
+            return View();
+        }
+    }
+}
