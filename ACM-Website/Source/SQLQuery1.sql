1ccfc9def7af6e6e10ca4c67f79bceee213cc13e
mwilliams10 <mwilliams10@augusta.edu>
2017-09-19 10:55:40 -0400

    started messing with database for website. project started to begin interactions with database initialization.

---
+++ ACM-Website/Source/SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE Users (
+uname varchar(255) NOT NULL UNIQUE,
+password_hash int NOT NULL,
+fname varchar(255) NOT NULL,
+lname varchar(255) NOT NULL
+);
+
+CREATE TABLE Admins (
+uname varchar(255) NOT NULL UNIQUE);
+
+CREATE TABLE Captains (
+uname varchar(255) NOT NULL UNIQUE);
---
+++ Website-Database Interaction/Website-Database Interaction/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+
+namespace Website_Database_Interaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectionStr = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\student\Source\Repos\personal - projects\ACM - Website\Source\ACM - Website\App_Data\localDB.mdf; Integrated Security = True";
+            while (true)
+            {
+                SqlConnection connection = new SqlConnection(connectionStr);
+                SqlCommand command;
+                SqlDataReader reader;
+                string sql;
+                Console.WriteLine("Input an SQL command to be executed");
+                sql = Console.ReadLine();
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection opened");
+                    command = new SqlCommand(sql, connection);
+                    Console.WriteLine("Command parsed");
+                    reader = command.ExecuteReader();
+                    Console.WriteLine("Reader executed");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetValue(0) + "-" + reader.GetValue(1) + "-" + reader.GetValue(2));
+                    }
+                    Console.WriteLine("Reader finished");
+                    reader.Close();
+                    command.Dispose();
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    connection.Close();
+                    Console.WriteLine("Something went wrong");
+                    Console.WriteLine(e.Message);
+                }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
